226925247cd9ea508388d1c2e62f00910e5fdd49
Madson Santos <madsonsamsung@gmail.com>
2025-03-08 10:46:32 -0300

    Fix description field type in AbilitySchema

--- src/models/Pokemon.ts
+++ src/models/Pokemon.ts
@@ -1,49 +1,50 @@
-import { Schema, model, Document } from 'mongoose';
+import { Schema, model, Document } from "mongoose";
 
-interface EnergyCost {    
-    colorless?: number;
-    grass?: number;
-    fire?: number;
-    water?: number;
-    lightning?: number;
-    psychic?: number;
-    fighting?: number;
-    darkness?: number;
-    metal?: number;
+interface EnergyCost {
+  colorless?: number;
+  grass?: number;
+  fire?: number;
+  water?: number;
+  lightning?: number;
+  psychic?: number;
+  fighting?: number;
+  darkness?: number;
+  metal?: number;
 }
 
 interface Attack {
-    name: string;
-    energyCost: EnergyCost[];
-    damage: number;
-    description: string;
+  name: string;
+  energyCost: EnergyCost[];
+  damage: number;
+  description: string;
 }
 
 interface Weakness {
-    type: string;
-    bonus: number;
+  type: string;
+  bonus: number;
 }
 
 interface Ability {
-    name: string;
-    description: number;
+  name: string;
+  description: string; // Correção aqui
 }
 
 interface PokemonCard extends Document {
-    name: string;
-    stage: string;
-    evolvesFrom: string;
-    hp: number;
-    type: string;
-    ability?: string;
-    attacks: Attack[];
-    weakness: Weakness;
-    retreatCost: number;
-    number: string;
-    exRule: string;
+  name: string;
+  stage: string;
+  evolvesFrom: string;
+  hp: number;
+  type: string;
+  ability?: Ability;
+  attacks: Attack[];
+  weakness: Weakness;
+  retreatCost: number;
+  number: string;
+  exRule: string;
 }
 
-const EnergyCostSchema = new Schema<EnergyCost>({  
+const EnergyCostSchema = new Schema<EnergyCost>(
+  {
     colorless: Number,
     grass: Number,
     fire: Number,
@@ -53,39 +54,50 @@ const EnergyCostSchema = new Schema<EnergyCost>({
     fighting: Number,
     darkness: Number,
     metal: Number,
-}, { _id: false });
+  },
+  { _id: false }
+);
 
-const AttackSchema = new Schema<Attack>({
+const AttackSchema = new Schema<Attack>(
+  {
     name: { type: String, required: true },
     energyCost: { type: [EnergyCostSchema], required: true },
     damage: { type: Number, required: true },
-    description: { type: String, default: "" }
-}, { _id: false });
+    description: { type: String, default: "" },
+  },
+  { _id: false }
+);
 
-const WeaknessSchema = new Schema<Weakness>({
+const WeaknessSchema = new Schema<Weakness>(
+  {
     type: { type: String, required: true },
-    bonus: { type: Number, required: true }
-}, { _id: false });
+    bonus: { type: Number, required: true },
+  },
+  { _id: false }
+);
 
-const AbilitySchema = new Schema<Ability>({
-    name: { type: String},
-    description: { type: Number}
-}, { _id: false });
+const AbilitySchema = new Schema<Ability>(
+  {
+    name: { type: String },
+    description: { type: String },
+  },
+  { _id: false }
+);
 
 const PokemonCardSchema = new Schema<PokemonCard>({
-    name: { type: String, required: true },
-    stage: { type: String, required: true },
-    evolvesFrom: { type: String, default: "" },
-    hp: { type: Number, required: true },
-    type: { type: String, required: true },
-    ability: { type: AbilitySchema, default: null },
-    attacks: { type: [AttackSchema], default: [] },
-    weakness: { type: WeaknessSchema, required: true },
-    retreatCost: { type: Number, default: null },
-    number: { type: String, default: ""},
-    exRule: { type: String, default: "" }
+  name: { type: String, required: true },
+  stage: { type: String, required: true },
+  evolvesFrom: { type: String, default: "" },
+  hp: { type: Number, required: true },
+  type: { type: String, required: true },
+  ability: { type: AbilitySchema, default: null },
+  attacks: { type: [AttackSchema], default: [] },
+  weakness: { type: WeaknessSchema, required: true },
+  retreatCost: { type: Number, default: null },
+  number: { type: String, default: "" },
+  exRule: { type: String, default: "" },
 });
 
-const PokemonCardModel = model<PokemonCard>('PokemonCard', PokemonCardSchema);
+const PokemonCardModel = model<PokemonCard>("PokemonCard", PokemonCardSchema);
 
 export default PokemonCardModel;
